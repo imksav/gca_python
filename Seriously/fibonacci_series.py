71e6a5a44071ab2f86d54155332625851dda7a76
Keshav Bhandari <imksav@gmail.com>
2021-05-08 19:02:02 +0545

    Added fibonacci series

---
+++ Seriously/fibonacci_series.py
@@ -0,0 +1,14 @@
+def custom_gen(a, b):
+    while True:
+        yield a
+        a, b = b, a+b
+
+
+n1 = int(input("Enter first number of a series:"))
+n2 = int(input("Enter second number of a series:"))
+t = int(input("Enter how many numbers of series you want to print:"))
+fibo = custom_gen(n1, n2)
+count = 1
+while count <= t:
+    count += 1
+    print(next(fibo))
